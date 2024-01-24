d39608a46fa2459d7a21d967e53f8107cc44b7bf
Cristobal Bergillos <cristobalbergillosnavarro@gmail.com>
2024-01-24 20:18:54 +0100

    Autocompletion, LSP, Snippets and transparent background

--- init.lua
+++ init.lua
@@ -14,21 +14,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	'tpope/vim-sleuth',
+	{ 
+		'rose-pine/neovim', 
+		name = "rose-pine",
+		config = function () 
+			require("rose-pine").setup({
+				styles = { transparency = true }
+			})
+			vim.cmd("colorscheme rose-pine")
+		end
+	},
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function () 
 			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, {})
 			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, {})
-			vim.keymap.set('n', '<leader>gs', require('telescope.builtin').grep_string, {})
+			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').grep_string, {})
 			vim.keymap.set('n', '<leader>lg', require('telescope.builtin').live_grep, {})
-		end
-	},
-	{ 
-		'rose-pine/neovim', 
-		name = "rose-pine",
-		config = function () 
-			vim.cmd("colorscheme rose-pine")
 		end
 	},
 	{
@@ -48,12 +52,6 @@ require("lazy").setup({
 				},
 			})
 		end
-	},
-	{
-		-- TODO create snippets for Flask
-		"L3MON4D3/LuaSnip",
-		version = "v2.*", 
-		build = "make install_jsregexp"
 	},
 	{
 		"mbbill/undotree",
@@ -81,10 +79,104 @@ require("lazy").setup({
 		config = function () 
 			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)		
 		end
+	},
+
+	-- LSP plugins
+	-- LSP Support
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	
+	-- Autocompletion
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	
+	-- Snippets
+	{ "rafamadriz/friendly-snippets" },
+	{
+		'L3MON4D3/LuaSnip',
+		dependencies = { "rafamadriz/friendly-snippets" } 
+	},
+})
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require("lspconfig").lua_ls.setup({})
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	handlers = {
+		lsp_zero.default_setup,
 	}
 })
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- [[ Configure nvim-cmp ]] From kickstart nvim: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+-- TODO add snippets here for Flask
 
+cmp.setup {
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	mapping = cmp.mapping.preset.insert {
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete {},
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'path' },
+	},
+}
+
+
+-- no background
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- terminal color
+vim.o.termguicolors = true
+
+-- file explorer
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
