d61c316b913d78c204f8717c75d5695f1732be8c
Cristobal Bergillos <cristobalbergillosnavarro@gmail.com>
2024-02-15 10:55:08 +0100

    Pyright setup

--- init.lua
+++ init.lua
@@ -110,16 +110,18 @@ lsp_zero.on_attach(function(client, bufnr)
 	lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require("lspconfig").lua_ls.setup({})
-
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
 	handlers = {
 		lsp_zero.default_setup,
-	}
+	},
+	ensure_installed = { "pyright" }
 })
+
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").pyright.setup({})
 
 -- [[ Configure nvim-cmp ]] From kickstart nvim: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 local cmp = require 'cmp'
@@ -184,6 +186,9 @@ vim.o.relativenumber = true
 -- save undo history
 vim.o.undofile = true
 
+-- column line at 88 chars
+vim.opt.colorcolumn="88"
+
 -- file explorer
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 
@@ -195,3 +200,4 @@ vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>P", '"+P')
+
